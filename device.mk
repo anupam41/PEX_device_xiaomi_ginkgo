8d794cbdab1a53ce55afc06a3062b6525069e578
Roberto Sartori <roberto.sartori.android@gmail.com>
2021-10-22 15:19:51 +0530

    ginkgo: disable compressed APEX
     Compressed APEX has been introduced in Android 12, and they are enabled
     by default inheriting updatable_apex.mk.
     Unfortunately, when the device is encrypted, they are causing bootloops.
    
     As the only advantage is some space gain on disk, we can
     simply disable them: keep the variable immediatly after including the
     updatable_apex.mk to avoid being overwritten.

--- device.mk
+++ device.mk
@@ -158,6 +158,10 @@ PRODUCT_PACKAGES += \
 # Enable updating of APEXes
 $(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
 
+# Disable APEX compression
+# Keep this after including updatable_apex.mk
+PRODUCT_COMPRESSED_APEX := false
+
 # Fingerprint
 PRODUCT_PACKAGES += \
     android.hardware.biometrics.fingerprint@2.1-service.ginkgo
